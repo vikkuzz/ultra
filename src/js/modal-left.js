2422a9bc61e22638d4c282ef0c8a371ec6fec0fd
Viktor Kuzminykh <simpat007@yandex.ru>
2020-07-02 23:55:47 +0300

    делал модалку

--- src/js/modal-left.js
+++ src/js/modal-left.js
@@ -1,6 +1,18 @@
 var buttonOpenMenu = document.querySelector('.nav__button-burger');
 var modalLeft = document.querySelector('.left-side-menu');
+var buttonCloseMenu = document.querySelector('.header-menu__button-left');
+var menu = document.querySelector('.left-side');
+
+buttonCloseMenu.addEventListener("click", function(){
+    modalLeft.style.heigth = '0';
+    modalLeft.style.width = '0';
+    modalLeft.style.left = '-400px';
+});
 
 buttonOpenMenu.addEventListener("click", function(){
-    modalLeft.style.display = 'flex';
+    modalLeft.style.position = 'absolute';
+    modalLeft.style.left = '0';
+    modalLeft.style.width = '100%';
+    modalLeft.style.heigth = '1024px';
+
 });
