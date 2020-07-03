7273a8466495ce942c0a52fef026ef927a46ec2c
Viktor Kuzminykh <simpat007@yandex.ru>
2020-07-04 01:25:28 +0300

    fix modal

--- src/js/modal-left.js
+++ src/js/modal-left.js
@@ -2,17 +2,21 @@ var buttonOpenMenu = document.querySelector('.nav__button-burger');
 var modalLeft = document.querySelector('.left-side-menu');
 var buttonCloseMenu = document.querySelector('.header-menu__button-left');
 var menu = document.querySelector('.left-side');
+var body = document.querySelector('.body');
+var fon = document.querySelector('.fon');
 
 buttonCloseMenu.addEventListener("click", function(){
-    modalLeft.style.heigth = '0';
-    modalLeft.style.width = '0';
     modalLeft.style.left = '-400px';
+    fon.style.position = 'static';
+    fon.style.height = '0';
+    body.style.height = '100%';
+    body.style.overflow = 'visible';
 });
 
 buttonOpenMenu.addEventListener("click", function(){
-    modalLeft.style.position = 'absolute';
     modalLeft.style.left = '0';
-    modalLeft.style.width = '100%';
-    modalLeft.style.heigth = '1024px';
-
+    fon.style.position = 'absolute';
+    fon.style.height = '1024px';
+    body.style.height = '1024px';
+    body.style.overflow = 'hidden';
 });
