ee1d9d4e5da8807b303bf98230959af40c20ee66
Viktor Kuzminykh <simpat007@yandex.ru>
2020-08-11 21:51:50 +0300

    jr

--- src/js/slider2-init.js
+++ src/js/slider2-init.js
@@ -1,35 +1,35 @@
-import Swiper from 'swiper';
+import Swiper from "swiper";
 
-const slider2 = document.querySelector('.swiper-container2');
+const slider2 = document.querySelector(".swiper-container2");
 
 let mySwiper2;
 
 function mobileSlider2() {
-    if (window.innerWidth <= 767 && slider2.dataset.mobile == 'false') {
-      mySwiper2 = new Swiper(slider2, {
-        slidesOffsetAfter: 0,
-        setWrapperSize: true,
-        width: 240,
-        spaceBetween: 16,
-        loop: true,
-        loopedSlides: 3,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-      });
-      slider2.dataset.mobile == 'true';
-    }
-    if (window.innerWidth > 767) {
-      slider2.dataset.mobile = 'false';
-      if (slider2.classList.contains('swiper-container-initialized')) {
-        mySwiper2.destroy();
-      }
+  if (window.innerWidth <= 767 && slider2.dataset.mobile == "false") {
+    mySwiper2 = new Swiper(slider2, {
+      slidesOffsetAfter: 0,
+      setWrapperSize: true,
+      width: 240,
+      spaceBetween: 16,
+      loop: true,
+      loopedSlides: 3,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+    slider2.dataset.mobile = "true";
+  }
+  if (window.innerWidth > 767) {
+    slider2.dataset.mobile = "false";
+    if (slider2.classList.contains("swiper-container-initialized")) {
+      mySwiper2.destroy();
     }
   }
-  
+}
+
+mobileSlider2();
+
+window.addEventListener("resize", () => {
   mobileSlider2();
-  
-  window.addEventListener('resize', () => {
-    mobileSlider2();
-  })
+});
